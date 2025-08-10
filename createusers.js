cffedeafc77ef828604226168daf0a87db0e3e58
Om Raheja <om@omraheja.me>
2025-08-10 12:49:39 -0400

    e

--- createusers.js
+++ createusers.js
@@ -3,14 +3,19 @@ require('dotenv').config();
 const bcrypt = require('bcrypt')
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'require' ? { rejectUnauthorized: false } : false
-});
+let pool = null;
+if (process.env.DB_STRING) {
+    pool = new Pool({connectionString: process.env.DB_STRING});
+} else {
+    pool = new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'require' ? { rejectUnauthorized: false } : false
+    });
+}
 
 // Test connection on startup
 pool.query('SELECT NOW()')
--- server.js
+++ server.js
@@ -16,15 +16,19 @@ const io = require("socket.io")(http, {
   maxHttpBufferSize: maxHttpBufferSizeInMb * 1024 * 1024,
 });
 
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'require' ? { rejectUnauthorized: false } : false
-});
+let pool = null;
+if (process.env.DB_STRING) {
+    pool = new Pool({connectionString: process.env.DB_STRING});
+} else {
+    pool = new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'require' ? { rejectUnauthorized: false } : false
+    });
+}
 
 pool.query('SELECT NOW()')
   .then(() => console.log('Database connected successfully'))
